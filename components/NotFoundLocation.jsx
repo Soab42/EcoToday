bef87d5b3fd1a8fecca408b2b2a1e080ac762cb1
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2024-04-23 14:30:10 +0600

    error page issue fix

--- components/NotFoundLocation.jsx
+++ components/NotFoundLocation.jsx
@@ -1,16 +1,21 @@
+"use client";
 import React from "react";
 import Card from "./Card";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function NotFoundLocation({ location }) {
+  const router = useRouter();
   return (
     <div class="col-span-12 flex flex-col justify-center items-center">
       <div className="bg-white w-[500px] text-center px-4 py-8 rounded-md">
         <div className="mb-4 text-2xl">{`Information for "${location}" not found!`}</div>
 
-        <Link href="/" className="text-blue-500 hover:underline">
+        <button
+          onClick={() => router.push("/")}
+          className="text-blue-500 hover:underline"
+        >
           Back to Home
-        </Link>
+        </button>
       </div>
     </div>
   );
